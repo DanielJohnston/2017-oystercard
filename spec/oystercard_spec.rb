63aeca589df00c087876f2e26d210e0fcda4d018
Daniel Whitston <dan@whitston.org.uk>
2017-02-14 14:11:27 +0000

    naive pass for max balance user story

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,5 +7,9 @@ describe Oystercard do
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'throws an error if exceeding the balance limit' do
+      expect { subject.top_up(500) }.to raise_error 'Max balance exceeded'
+    end
   end
 end
